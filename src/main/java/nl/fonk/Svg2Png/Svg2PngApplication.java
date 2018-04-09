2a8dd01a1974c4a6896ce2a564977effd3fd0e81
Adam Semenenko <adam@fonk.amsterdam>
2018-04-09 11:04:56 +0200

    First commit

---
+++ src/main/java/nl/fonk/Svg2Png/Svg2PngApplication.java
@@ -0,0 +1,12 @@
+package nl.fonk.Svg2Png;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Svg2PngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Svg2PngApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/fonk/Svg2Png/Svg2PngApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.fonk.Svg2Png;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Svg2PngApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
